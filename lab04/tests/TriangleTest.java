241ee3d398da683e49810faa179d46f1c87c81bd
LiuZhaoyang <1343183241@qq.com>
2025-09-20 14:09:05 +0800

    finished lab04

--- lab04/tests/TriangleTest.java
+++ lab04/tests/TriangleTest.java
@@ -17,8 +17,37 @@ public abstract class TriangleTest {
         Triangle t = getNewTriangle();
         // remember that you'll have to call on Triangle methods like
         // t.functionName(arguments), where t is a Triangle object
-
+        assertWithMessage("They construct a triangle.").that(t.sidesFormTriangle(2,2,3)).isTrue();
+        assertWithMessage("They cannot construct a triangle.").that(t.sidesFormTriangle(1,2,3)).isFalse();
+        assertWithMessage("They cannot construct a triangle.").that(t.sidesFormTriangle(3,0,4)).isFalse();
+        assertWithMessage("They cannot construct a triangle.").that(t.sidesFormTriangle(0,0,0)).isFalse();
     }
 
+    @Test
+    public void test2() {
+        // TODO: stub for second test
+        Triangle t = getNewTriangle();
+        assertWithMessage("They construct a triangle.").that(t.pointsFormTriangle(0,0,3,0,0,4)).isTrue();
+        assertWithMessage("They cannot construct a triangle.").that(t.pointsFormTriangle(0,0,1,1,2,2)).isFalse();
+        assertWithMessage("They cannot construct a triangle.").that(t.pointsFormTriangle(0,0,0,0,0,0)).isFalse();
+    }
+
+    @Test
+    public void test3() {
+        // TODO: stub for third test
+        Triangle t = getNewTriangle();
+        assertWithMessage("They form an Scalene triangle.").that(t.triangleType(2,3,4)).isEqualTo("Scalene");
+        assertWithMessage("They form an Isosceles triangle.").that(t.triangleType(2,2,3)).isEqualTo("Isosceles");
+        assertWithMessage("They form an Equilateral triangle.").that(t.triangleType(2,2,2)).isEqualTo("Equilateral");
+    }
+
+    @Test
+    public void test4() {
+        // TODO: stub for fourth test
+        Triangle t = getNewTriangle();
+        assertWithMessage("The squared hypotenuse is 25.").that(t.squaredHypotenuse(3,4)).isEqualTo(25);
+        assertWithMessage("The squared hypotenuse is 13.").that(t.squaredHypotenuse(2,3)).isEqualTo(13);
+        assertWithMessage("The squared hypotenuse is 0.").that(t.squaredHypotenuse(0,0)).isEqualTo(0);
+    }
 
 }
