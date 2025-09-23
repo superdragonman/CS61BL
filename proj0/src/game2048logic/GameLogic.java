36039a1cc52a4a171a93d1e0213e6c6a34643bd3
LiuZhaoyang <1343183241@qq.com>
2025-09-23 10:51:03 +0800

    finished project0

--- proj0/src/game2048logic/GameLogic.java
+++ proj0/src/game2048logic/GameLogic.java
@@ -20,7 +20,38 @@ public class GameLogic {
      */
     public static int moveTileUpAsFarAsPossible(int[][] board, int r, int c, int minR) {
         // TODO: Fill this in in tasks 2, 3, 4
-        return 0;
+        if (board[r][c] != 0 && r != 0){
+            int i = r - 1;
+            if (i < minR){
+                return minR;
+            }
+            while(i >= minR){
+                if (board[i][c] == 0){
+                    i--;
+                    continue;
+                }
+                break;
+            }
+            if (i < minR){
+                board[minR][c] = board[r][c];
+                board[r][c] = 0;
+                return minR;
+            }
+            else if (board[i][c] == board[r][c]){
+                board[i][c] = board[i][c]*2;
+                board[r][c] = 0;
+                return i + 1;
+            }else {
+                board[i + 1][c] = board[r][c];
+                if (i + 1 != r){
+                    board[r][c] = 0;
+                }
+                return minR;
+            }
+        }
+        else{
+            return minR;
+        }
     }
 
     /**
@@ -32,6 +63,15 @@ public class GameLogic {
      */
     public static void tiltColumn(int[][] board, int c) {
         // TODO: fill this in in task 5
+        int size = board.length;
+        int minR = 0;
+        for (int i = 0; i < size; i++) {
+//           moveTileUpAsFarAsPossible(board, i, c, minR);
+//            if (moveTileUpAsFarAsPossible(board, i, c, minR) != minR){
+//                minR = moveTileUpAsFarAsPossible(board, i, c, minR);
+// 不要多次调用moveTileUpAsFarAsPossible，应该只调用一次并存储结果
+            minR = moveTileUpAsFarAsPossible(board, i, c, minR);
+        }
         return;
     }
 
@@ -42,6 +82,10 @@ public class GameLogic {
      */
     public static void tiltUp(int[][] board) {
         // TODO: fill this in in task 6
+        int size = board.length;
+        for (int i = 0; i < size; i++) {
+            tiltColumn(board, i);
+        }
         return;
     }
 
@@ -55,12 +99,24 @@ public class GameLogic {
     public static void tilt(int[][] board, Side side) {
         // TODO: fill this in in task 7
         if (side == Side.NORTH) {
+            tiltUp(board);
             return;
         } else if (side == Side.EAST) {
+            rotateLeft(board);
+            tiltUp(board);
+            rotateRight(board);
             return;
         } else if (side == Side.SOUTH) {
+            rotateLeft(board);
+            rotateLeft(board);
+            tiltUp(board);
+            rotateRight(board);
+            rotateRight(board);
             return;
         } else if (side == Side.WEST) {
+            rotateRight(board);
+            tiltUp(board);
+            rotateLeft(board);
             return;
         } else {
             System.out.println("Invalid side specified");
