4a190cfc028c42d4a84eaa2d4dcceade023b4a0f
LiuZhaoyang <1343183241@qq.com>
2025-09-24 19:19:56 +0800

    finished project1a

---
+++ proj1a/src/LinkedListDeque61B.java
@@ -0,0 +1,115 @@
+import java.util.List;
+import java.util.ArrayList; // import the ArrayList class
+
+public class LinkedListDeque61B<T> implements Deque61B<T>{
+
+    public Node sentinel;
+    public int size;
+
+    public LinkedListDeque61B() {
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+    public class Node {
+        public T value;
+        public Node prev;
+        public Node next;
+
+        public Node(T value, Node prev, Node next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+
+    @Override
+    public void addFirst(T x) {
+        Node newNode = new Node(x, sentinel, sentinel.next);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size++;
+    }
+
+    @Override
+    public void addLast(T x) {
+        Node newNode = new Node(x, sentinel.prev, sentinel);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size++;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> List = new ArrayList<>();
+        Node current = sentinel.next;
+        while (current != sentinel) {
+            List.add(current.value);
+            current = current.next;
+        }
+        return List;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return sentinel.next == sentinel;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        T firstValue = sentinel.next.value;
+        sentinel.next.next.prev = sentinel;
+        sentinel.next = sentinel.next.next;
+        size--;
+        return firstValue;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        T lastValue = sentinel.prev.value;
+        sentinel.prev.prev.next = sentinel;
+        sentinel.prev = sentinel.prev.prev;
+        size--;
+        return lastValue;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        Node current = sentinel.next;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.value;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        } else if (index == 0) {
+            return sentinel.next.value;
+        } else {
+            LinkedListDeque61B<T> subDeque = new LinkedListDeque61B<>();
+            subDeque.sentinel = this.sentinel.next;
+            subDeque.size = this.size - 1;
+            return subDeque.getRecursive(index - 1);
+        }
+    }
+}
